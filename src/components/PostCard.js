f5f9bd05fe0e2ef50130659233fa72a77b976ed9
Luis Hernández <ldhdv95@gmail.com>
2018-09-26 09:12:41 -0500

    Show posts

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import {Item, Button} from 'semantic-ui-react';
+import {Item, Button, Image} from 'semantic-ui-react';
 
 const PostCard = (props) => {
   return (
-    <Item.Group>
-      <Item>
-        <Item.Image  />
-        <Item.Content>
-          <Item.Header as='a'><h2>Post Title</h2></Item.Header>
-          <Item.Meta>Votes: 60</Item.Meta>
-          <Item.Description>
-            <p>Post content</p>
-          </Item.Description>
-          <Item.Extra>
-            <Button circular icon='thumbs up outline' color='blue'></Button>
-            <Button circular icon='thumbs down outline'></Button>
-          </Item.Extra>
-        </Item.Content>
-      </Item>
-    </Item.Group>
+    <Item>
+      <Item.Image  src={props.data.post_image_url}/>
+      <Item.Content>
+        <Item.Header as='a'>
+          <h2>
+            <a target='blank' href={props.data.url}>{props.data.title}</a>
+          </h2>
+        </Item.Header>
+        <Item.Meta>
+          <div style={{display: 'inlineBlock'}}>
+            Votes: {props.data.votes}
+            <span style={{marginLeft: 10}}>Writer: </span>
+            <Image src={props.data.writer_avatar_url} avatar />
+          </div>
+        </Item.Meta>
+        <Item.Description>
+          <p>{props.data.description}</p>
+        </Item.Description>
+        <Item.Extra>
+          <Button circular icon='thumbs up outline' color='blue'></Button>
+          <Button circular icon='thumbs down outline'></Button>
+        </Item.Extra>
+      </Item.Content>
+    </Item>
   )
 }
 
